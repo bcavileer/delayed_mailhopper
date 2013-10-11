7059fd269b12cf838b5ef5a6298ae616308b8a54
Benjamin Cavileer <bcavileer@gmail.com>
2013-10-11 15:05:16 -0400

    Bump email priority

--- app/models/delayed_mailhopper/email.rb
+++ app/models/delayed_mailhopper/email.rb
@@ -4,9 +4,9 @@ require 'delayed_job'
 module DelayedMailhopper
   class Email < Mailhopper::Email
     after_create :enqueue
-
+    
     def enqueue
-      Delayed::Job.enqueue(SendJob.new(self.id))
+      Delayed::Job.enqueue(SendJob.new(self.id), :priority => 5)
     end
   end
 end
